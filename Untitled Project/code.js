e5ef811cdebe7902fcc87587ef4998e61706873a
RhythmAgarwal1908 <66068059+RhythmAgarwal1908@users.noreply.github.com>
2020-12-03 10:42:06 +0530

    Add files via upload

---
+++ Untitled Project/code.js
@@ -0,0 +1,9 @@
+onEvent("Searchbtn", "click", function( ) {
+	var searchInput = getText("SearchBox");
+	if (searchInput === "Tulsi") {
+	  setScreen("screen2");
+	}
+	if (searchInput === "tulsi") {
+	  setScreen("screen2");
+	}
+});
